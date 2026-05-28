2d4dfc49091e104365e3f38e30baae32de25c5cc
GaleazziI <joaogaleazzi14@gmail.com>
2026-05-28 20:17:19 -0300

    Add files via upload

---
+++ T2.pl
@@ -0,0 +1,20 @@
+autor('Machado de Assis', 'Brasileira').
+autor('J. K. Rowling', 'Britanica').
+autor('George Orwell', 'Britanica').
+autor('J. R. R. Tolkien', 'Britanica').
+autor('Sun Tzu', 'Chinesa').
+
+livro('Dom Casmurro', 'Machado de Assis', 1899, 'Romance').
+livro('Harry Potter e a Pedra Filosofal', 'J. K. Rowling', 1997, 'Fantasia').
+livro('1984', 'George Orwell', 1949, 'Ficcao').
+livro('O Senhor dos Aneis', 'J. R. R. Tolkien', 1954, 'Fantasia').
+livro('A Arte da Guerra', 'Sun Tzu', 500, 'Estrategia').
+
+
+pessoa('Maria Silva', 101).
+pessoa('Joao Pedro', 102).
+pessoa('Ana Souza', 103).
+
+
+emprestado('1984', 101, '2026-05-20').
+emprestado('Dom Casmurro', 102, '2026-05-25').
